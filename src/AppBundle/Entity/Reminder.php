f3b062c304a6c5244dfbe49bafbeb27d18856f96
jakub <gaboryjakub@gmail.com>
2018-01-08 21:02:27 +0100

    Create Enities

---
+++ src/AppBundle/Entity/Reminder.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reminder
+ *
+ * @ORM\Table(name="reminder")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReminderRepository")
+ */
+class Reminder
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="reminders")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="series_id", type="integer")
+     */
+    private $seriesId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="notification_type", type="smallint")
+     */
+    private $notificationType;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set seriesId
+     *
+     * @param integer $seriesId
+     *
+     * @return Reminder
+     */
+    public function setSeriesId($seriesId)
+    {
+        $this->seriesId = $seriesId;
+
+        return $this;
+    }
+
+    /**
+     * Get seriesId
+     *
+     * @return int
+     */
+    public function getSeriesId()
+    {
+        return $this->seriesId;
+    }
+
+    /**
+     * Set notificationType
+     *
+     * @param integer $notificationType
+     *
+     * @return Reminder
+     */
+    public function setNotificationType($notificationType)
+    {
+        $this->notificationType = $notificationType;
+
+        return $this;
+    }
+
+    /**
+     * Get notificationType
+     *
+     * @return int
+     */
+    public function getNotificationType()
+    {
+        return $this->notificationType;
+    }
+}
+
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Reminder", mappedBy="user")
+     */
+    private $reminders;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=127, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hashed_password", type="string", length=255, nullable=true)
+     */
+    private $hashedPassword;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salt", type="string", length=255, nullable=true)
+     */
+    private $salt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set hashedPassword
+     *
+     * @param string $hashedPassword
+     *
+     * @return User
+     */
+    public function setHashedPassword($hashedPassword)
+    {
+        $this->hashedPassword = $hashedPassword;
+
+        return $this;
+    }
+
+    /**
+     * Get hashedPassword
+     *
+     * @return string
+     */
+    public function getHashedPassword()
+    {
+        return $this->hashedPassword;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     *
+     * @return User
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+}
+
