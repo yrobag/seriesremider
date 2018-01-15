184a27789a3661a737e298f15ee4608ccb341e79
jakub <gaboryjakub@gmail.com>
2018-01-15 21:32:40 +0100

    Dynamic fetching data from external api

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,9 +5,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Service\UserService;
 
 
 /**
